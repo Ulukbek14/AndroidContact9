3c879d29b2d49b976c1b8868cd1a1be86520b5e3
Ulukbek14 <ulukbekkylycbekov@gmail.com>
2021-05-23 18:22:23 +0600

    Обновил

---
+++ app/src/main/java/com/example/androidcontact9/CallMe.java
@@ -0,0 +1,5 @@
+package com.example.androidcontact9;
+
+public interface CallMe {
+    void Callme(int position);
+}
---
+++ app/src/main/java/com/example/androidcontact9/ContactModel.java
@@ -0,0 +1,23 @@
+package com.example.androidcontact9;
+
+public class ContactModel {
+    //Initialize variable
+    String name,number;
+    //Generate getter and setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
